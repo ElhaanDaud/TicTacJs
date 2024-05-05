bc5f8df88ef5bb48799cc1ebee77cdadfa4b4dca
ElhaanDaud <elhaan.daud786@gmail.com>
2024-05-05 23:17:12 +0530

    restart func

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 let playerText= document.getElementById('playerText')
-let restartBtn = document.getElementById('retartBtn')
+let restartBtn = document.getElementById('restartBtn')
 let boxes = Array.from(document.getElementsByClassName('box'))
 
 console.log(boxes)
@@ -12,3 +12,32 @@ let currentPlayer = X_Text
 let spaces = Array(9).fill(null)
 
 console.log(spaces)
+
+const startGame = () => {
+    boxes.forEach(box=>box.addEventListener('click',boxClicked))
+}
+
+function boxClicked(e){
+    const id = e.target.id
+
+    if(!spaces[id]){
+        spaces[id]== currentPlayer
+        e.target.innerText=currentPlayer
+
+        currentPlayer= currentPlayer ==X_Text ? O_Text : X_Text /* if statement hai ye */
+
+    }
+
+}
+
+restartBtn.addEventListener('click',restart)
+
+function restart(){
+    spaces.fill(null)
+    boxes.forEach(box=>{
+        box.innerText=''
+    })
+    currentPlayer= X_Text
+}
+
+startGame()
